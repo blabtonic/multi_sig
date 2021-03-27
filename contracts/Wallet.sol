a69d558bc2b9a6d50a558c2e0d5f4d55b1b05a83
Trystan Hendricks <dolkhound@gmx.com>
2021-03-27 12:20:16 -0400

    created constructor for wallet contract

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -6,7 +6,7 @@ contract Wallet {
   event SubmitTransaction(
     address indexed owner,
     address indexed to,
-    uint256 value,
+    uint256 amount,
     uint256 indexed txIndex,
     bytes data
   );
@@ -15,6 +15,40 @@ contract Wallet {
   event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
   
   address[] public owners;
+  // Number of confirmations needed for transaction to go through
+  uint256 public Confirmations;
+  
+  struct Transaction {
+    uint256 amount;
+    address to;
+    bytes data;
+    bool executed;
+    mapping(address => bool) isConfirmed;
+    uint256 numConfirmations;
+  }
+
+  Transaction[] public transactions;
+  
+  constructor(address[] memory _owners, uint256 _numConfirmationsRequired) public {
+    require(_owners.length > 0, "owners required");
+    require(
+      _numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length, 
+      "invalid number of required confirmations"
+    );
+
+    for (uint256 i = 0; i < _owners.length; i++) {
+      address owner = _owners[i];
+
+      require(owner != address(0), "invalid owner");
+      // Error check for unique owners to approve
+      //require(!isOwner[owner], "owner not unique");
+      
+      //isOwner[owner] = true;
+      owners.push(owner);
+    }
+    
+    _numConfirmationsRequired = _numConfirmationsRequired;
+  }
 
   function submitTransaction() public {}
 
