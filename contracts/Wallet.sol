21d7ddd56708de0d5041bb1b6952e5ce6b7e01e9
Trystan Hendricks <dolkhound@gmx.com>
2021-03-27 20:49:53 -0400

    submitTransaction added

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -14,7 +14,10 @@ contract Wallet {
   event RevokeConfirmation(address indexed owner, uint256 indexed txIndex);
   event ExecuteTransaction(address indexed owner, uint256 indexed txIndex);
   
+  // Always set an owner for contract
   address[] public owners;
+  mapping (address => bool) isOwner;
+  
   // Number of confirmations needed for transaction to go through
   uint256 public Confirmations;
   
@@ -23,9 +26,11 @@ contract Wallet {
     address to;
     bytes data;
     bool executed;
-    mapping(address => bool) isConfirmed;
     uint256 numConfirmations;
   }
+
+  // Tx index => owner => bool
+  mapping(address => bool) public isConfirmed;
 
   Transaction[] public transactions;
   
@@ -41,16 +46,33 @@ contract Wallet {
 
       require(owner != address(0), "invalid owner");
       // Error check for unique owners to approve
-      //require(!isOwner[owner], "owner not unique");
+      require(!isOwner[owner], "owner not unique");
       
-      //isOwner[owner] = true;
+      isOwner[owner] = true;
       owners.push(owner);
     }
     
     _numConfirmationsRequired = _numConfirmationsRequired;
   }
 
-  function submitTransaction() public {}
+  modifier onlyOwner() {
+    require(isOwner[msg.sender], "not owner");
+    _;
+  }
+
+  function submitTransaction(address _to, uint256 _amount, bytes memory _data) public onlyOwner {
+    uint256 txIndex = transactions.length;
+
+    transactions.push(Transaction({
+      to: _to,
+      amount: _amount,
+      data: _data,
+      executed: false,
+      numConfirmations: 0
+    }));
+
+    emit SubmitTransaction(msg.sender, _to, _amount, txIndex, _data);
+  }
 
   function confirmTransaction() public {}
 
